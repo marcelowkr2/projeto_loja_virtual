1a7bfc904e0b83ed97b7d05a3c54b836f37ed009
Marcelo Pires de Oliveira <119169432+marcelowkr2@users.noreply.github.com>
2024-08-06 18:33:58 -0300

    first commit

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+// router.php
+if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
+    return false;    // serve the requested resource as-is.
+} else {
+   session_start();
+
+    define('DEFAULT_CONTROLLER', 'home');
+    define('DEFAULT_METHOD', 'index');
+
+   require '../vendor/autoload.php';
+   require '../app/Functions/functions_twig.php';
+   require 'bootstrap/bootstrap.php';
+
+}
+
